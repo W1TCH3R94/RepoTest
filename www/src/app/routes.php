081f40a66aafdd586ca719d428a620eb72fcac32
w1tch3r94 <jeduardo1394@gmail.com>
2024-05-14 17:34:23 -0600

    Se habilitan las rutas update, delete y filtrar

--- www/src/app/routes.php
+++ www/src/app/routes.php
@@ -8,4 +8,7 @@ use Slim\Routing\RouteCollectorProxy;
 $app->group('/cliente',function(RouteCollectorProxy $cliente){
     $cliente->post('', Cliente::class . ':create');
     $cliente->get('/read[/{id}]', Cliente::class . ':read');
+    $cliente->get('/filtro', Cliente::class . ':filtrar');
+    $cliente->put('/{id}', Cliente::class . ':update');
+    $cliente->delete('/{id}', Cliente::class . ':delete');
 });
